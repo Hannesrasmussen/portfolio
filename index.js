25ee76eb794a230d4ea286da5f4d447eb393b0d0
hannesrasmussen <hannes.e.rasmussen@gmail.com>
2023-01-23 22:12:21 +0100

    V3.1

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const body = document.body;
 const header = document.getElementById("header");
 const aboutElem = document.getElementById("about");
-const logo = document.getElementById("logo");
-var projects = document.getElementsByClassName("project");
-var popup = document.getElementById("popup-message");
-var popupBtn = document.getElementById("popup-button");
+const extras = document.getElementsByClassName("extras-img");
+
+const popupdarkness = document.getElementById("popup-container");
+const closebutton = document.getElementById("close-button");
+const popupimg = document.getElementById("popup-img");
 
 
 var previousY = window.scrollY;
@@ -11,34 +13,38 @@ var percentage;
 
 // Event Listeners:
 header.addEventListener("mouseover",visibleHeader)
-logo.addEventListener("click",goUp)
-for (i = 0; i < projects.length; i++){
-    projects[i].addEventListener("click",(e)=>{
-        if(e.currentTarget.dataset.adress){
-            window.open(e.currentTarget.dataset.adress)
-        } else {
-            popup.style.opacity = "100%";
-            popup.style.pointerEvents = "all";
-            
-        }
-    })
-}
-popupBtn.addEventListener("click",function(){
-    popup.style.opacity = "0%";
-    popup.style.pointerEvents = "none";
-})
 
 // Callbacks
 function visibleHeader() {
     header.style.opacity = "100%";
 }
 
-function goUp() {
-    window.scrollBy({
-        top: -100000,
-        behavior: 'smooth'
-      });
+for (i = 0; i < extras.length; i++) {
+    extras[i].addEventListener("click", (e) => {{
+        popupdarkness.style.display = "block";
+        popupimg.src = e.target.src;
+        header.style.display = "none";
+
+        popupdarkness.addEventListener("click",(e) => {
+            header.style.display = "flex";
+            popupdarkness.style.display = "none";
+        })
+        closebutton.addEventListener("click",(e) => {
+            header.style.display = "flex";
+            popupdarkness.style.display = "none";
+        })
+        
+    }})
+    console.log(popupdarkness);
 }
+
+
+
+
+body.addEventListener("mousemove", (e) => {
+  body.style.backgroundPositionX = -e.offsetX + "px";
+  body.style.backgroundPositionY = -e.offsetY + "px";
+});
 
 function scrollFunction() {
     percentage = Math.round(window.scrollY * 0.1)
@@ -50,6 +56,54 @@ function scrollFunction() {
     previousY = window.scrollY;
 }
 
-
-
 window.onscroll = scrollFunction;
+
+addEventListener("resize", () => {
+    let y;
+    clearTimeout(y);
+    let x = $('#site-landing');
+    
+    x[0].style.transition = "0.2s";
+    x[0].style.opacity = "0%";
+    
+    y = setTimeout(() => {
+        x[0].style.opacity = "100%";
+        x[0].style.transition = "0.2s";
+    }, 1000);
+    
+    $('#site-landing').polygonizr({
+        canvasWidth: $("body").width(),
+        canvasHeight: $("body").height(),
+    })
+});
+
+$('#site-landing').polygonizr({
+    numberOfNodes: 60,
+    nodeEase: 'linear',
+    duration: 10,
+    // If set to true, the animation will rotate. Default: false
+    node3dRotate: false,
+    // If node3dRotate is set to true, the following option indicate if rotation should pause between n restNodeMovements. Default: 1
+    node3dRotateOnNthNodeMovement: 6,
+    // If node3dRotate is set to true, the following option indicate the alpha of the nodes at the far end of the rotation, creating depth. Default: 0.1
+    node3dRotateDepthAlpha: 0.8,
+    // If node3dRotate is set to true, the following option indicates the ease mode of each node movement (linear, easeIn, easeOut, easeInOut, accelerateDecelerate). Default: linear
+    node3dRotatEase: "linear",
+    // If node3dRotate is set to true, the following option indicate the axis on the canvas around which the animation will rotate (median, center, left, right). Default: center
+    node3dRotateAxis: "center",
+
+    // Indicates the fill color's alpha level (1-0). Default: 0.5
+    nodeFillAlpha: 0.05,
+    // Indicates the alpha level (1-0) of the line drawn between connected nodes. Default: 0.5
+    nodeLineAlpha: 0.05,
+    // Indicates the alpha level (1-0) of each node's "dot". Default: 1.0
+    nodeDotAlpha: 0.05,
+
+    // Indicates the width of the canvas on which to paint each node. Default: $(this).width()
+    canvasWidth: $(this).width(),
+    // Indicates the height of the canvas on which to paint each node. Default: $(this).height();
+    canvasHeight: $(this).height(),
+
+    // Indicate the CSS z-index property by which to specify the stack order of the canvas. Default: "auto"
+    canvasZ: "1"
+});
