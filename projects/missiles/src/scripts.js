1d13988d185359ed3e8a5a577080bb5b683695f8
hannesrasmussen <hannes.e.rasmussen@gmail.com>
2024-02-28 17:39:21 +0100

    about text changes and projects

--- projects/missiles/src/scripts.js
+++ projects/missiles/src/scripts.js
@@ -3,14 +3,7 @@ const content = document.getElementById("content");
 const cover = document.getElementById('album-element-cover');
 
 function init() {
-    content.addEventListener("mousemove", (e) => {
-        const rect = e.currentTarget.getBoundingClientRect();
-        const offsetX = e.clientX - rect.left;
-        const offsetY = e.clientY - rect.top;
-        
-        background.style.backgroundPositionX = -offsetX / 35 + "px";
-        background.style.backgroundPositionY = -offsetY / 35 + "px";
-    });
+    
 }
 
 window.onload = init();
